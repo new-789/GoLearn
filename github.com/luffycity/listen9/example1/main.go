235f4516af2fa7a6a951a3064f87f0e1135d460d
zhufeng <zhuxiujian@foxmail.com>
2020-03-02 08:57:14 +0800

    2020-03-02

--- github.com/luffycity/listen9/example1/main.go
+++ github.com/luffycity/listen9/example1/main.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main() {
+	// make([]string, 5, 10) 表示创建的字符串切片包含5个空元素，10 则表示它所引用的字符串数组长度是等于10
 	var sa []string = make([]string, 5, 10)
-	fmt.Printf("sa:%v\n", sa)
+	fmt.Println(sa)
 	for i := 0; i < 10; i++ {
+		// fmt.Sprintf 格式化输出整数类型数据并将其转换为字符串类型的数据
 		sa = append(sa, fmt.Sprintf("%d", i))
 	}
-	fmt.Println("sa:", sa)
+	fmt.Println(sa)
 }

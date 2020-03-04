b26714a41effc3f0e81cc02282de481bb79a5153
zhufeng <zhuxiujian@foxmail.com>
2020-03-04 20:26:19 +0800

    增加了自定义包代码和 package 组织方式代码

--- github.com/luffycity/listen11/custom/custom.go
+++ github.com/luffycity/listen11/custom/custom.go
@@ -1,5 +1,7 @@
 package custom
 
+import "fmt"
+
 var (
 	Sum int
 )
@@ -7,4 +9,8 @@ var (
 func Add(a int, b int) int {
 	Sum = a + b
 	return Sum
+}
+
+func init() {
+	fmt.Println("这是 custom 包中的 init 函数")
 }
--- github.com/luffycity/listen11/custom_test/main.go
+++ github.com/luffycity/listen11/custom_test/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
-	// 引入自定义的静态库文件(包)
-	"github.com/luffycity/listen11/custom"
+	// 引入自定义的静态库文件(包),前面加上下划线表示我们没有导入该包中的任何变量或是函数，
+	// 但是我们有需要该包中的 init 函数来为我们做初始化，此时我们就可以在前面加上 _ 下划线
+	_ "github.com/luffycity/listen11/custom"
 )
+
+func init() {
+	fmt.Println("程序启动时自定调用该 init 函数")
+}
+
+func init() {
+	fmt.Println("程序启动时自定调用该 init2 函数")
+}
 
 func main() {
 	// 调用自定义包中的 Add 函数并进行传参
-	var sum int = custom.Add(10, 70)
+	//var sum int = custom.Add(10, 70)
 	fmt.Printf("sum:%d\n", sum)
 }
